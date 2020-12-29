0cd07a2c8843d47065804b134066c2bbc1dea0de
Fabio Steffen <35061965+fdsteffen@users.noreply.github.com>
2020-12-29 16:03:34 +0100

    Add sphinx demo page

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## Changelog
+
+### [1.0.0] 2020-12-29
+#### Added
+- initial release of Bib2HTML
+- github page with demo citations
---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('../../'))
+
+
+project = 'bib2html'
+copyright = '2020, Fabio Steffen'
+author = 'Fabio Steffen'
+release = '1.0.0'
+
+extensions = [
+]
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_show_sourcelink = False
+
+html_theme_options = {
+    'font_family': 'Open Sans',
+    'logo':'logo.png',
+    }
+
+html_sidebars = {
+    '**': [
+        'about.html',
+    ]
+}
